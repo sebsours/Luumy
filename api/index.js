b1bdc38783e2cecd12ec5d399d23da8afadc1792
sebsours <sebsours@gmail.com>
2023-05-11 02:49:00 -0400

    Rearranged the apis

--- api/index.js
+++ api/index.js
@@ -1,10 +1,21 @@
-const express = require('express');
-const app = express();
-const cors = require('cors');
+import express from "express";
+import cors from 'cors';
+import dotenv from 'dotenv';
+import spotify_apis from "./routes/spotify.js";
+dotenv.config();
 
-const port = 8000;
+const app = express();
+
+
+
+const port = process.env.PORT;
+// const spotify_token_data = await getAuth();
 
 app.use(cors());
+app.use(express.json());
+
+// endpoints that use the spotify api
+app.use("/spotify", spotify_apis);
 
 app.get('/', (req, res) => {
     res.send('OMG IS THIS SHIT ACTUALLY WORKING?');
@@ -12,4 +23,5 @@ app.get('/', (req, res) => {
 
 app.listen(port, () => {
     console.log(`API listening on port ${port}`);
+    // console.log(`Access token is: ${spotify_token_data.access_token}`);
 })
---
+++ api/routes/spotify.js
@@ -0,0 +1,48 @@
+// PUT ALL DYNAMIC ROUTES AT THE BOTTOM so things dont break
+import axios from "axios";
+import dotenv from 'dotenv';
+import express from 'express';
+
+dotenv.config();
+const router = express.Router();
+
+const client_id = process.env.SPOTIFY_CLIENT_ID;
+const client_secret = process.env.SPOTIFY_CLIENT_SECRET;
+const auth_token = Buffer.from(client_id + ":" + client_secret, 'utf-8').toString('base64');
+
+// Get the access_token to access the other spotify APIs
+// !! NOTE access token expires after 1 hour
+const getAuth = async () => {
+    const url = 'https://accounts.spotify.com/api/token';
+    try {
+        const response = await axios.post(url, {
+            grant_type: 'client_credentials'
+        }, {
+            headers: {
+                'Authorization': 'Basic ' + auth_token,
+                'Content-Type': 'application/x-www-form-urlencoded',
+            }
+        })
+        // console.log(response.data);
+        return response.data.access_token;
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+router.get('/search/:query', async (req, res, next) => {
+    // res.send(`Trying to find: ${req.params.query}`);
+    const url = 'https://api.spotify.com/v1/search?q=' + req.params.query + '&type=album';
+    // res.send('Bearer ' + await getAuth());
+    await axios.get(url, {
+        headers: {
+            'Authorization': 'Bearer ' + await getAuth(),
+            'Content-Type': 'application/json'
+        }
+    })
+        .then(response => res.send(response.data))
+        .catch(error => console.log(error));
+    // res.send(`Searching for... ${req.params.query}`);
+})
+
+export default router;
